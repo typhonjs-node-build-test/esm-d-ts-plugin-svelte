36a66467553d3b09173867aa4cbf61b3cb29d8b5
typhonrt <support@typhonjs.io>
2024-01-27 13:55:48 -0800

    Add support for the `compile:diagnostic:filter` event.

--- src/DTSPluginSvelte.js
+++ src/DTSPluginSvelte.js
@@ -32,15 +32,146 @@ export class DTSPluginSvelte
    static #regexScriptIsTS = /<script\s+[^>]*?lang=('|")(ts|typescript)('|")/;
 
    /**
+    * Detects an intermediate DTS Svelte filename extension.
+    *
+    * @type {RegExp}
+    */
+   static #regexSvelteFile = /\.svelte\.(js|ts)$/;
+
+   /**
     * Matches spurious `@typedef` statements output from `svelte2tsx` for removal.
     *
     * @type {RegExp}
     */
    static #regexTypedef = /\/\*\*[\s\S]*?@typedef[\s\S]*?__propDef[\s\S]*?\*\/\s*/g;
 
+   /**
+    * Stores the relative intermediate declaration Svelte file path to parsed component JSDoc.
+    *
+    * @type {Map<string, import('./JSDocCommentParser.js').ComponentJSDoc>}
+    */
    #componentComments = new Map();
 
    // Plugin event callbacks -----------------------------------------------------------------------------------------
+
+   /**
+    * Filters raised diagnostic messages from the Typescript compiler for Svelte components. There can be some noisy
+    * warnings from `svelte2tsx` output depending on the complexity of the component. These diagnostic messages are
+    * moved to the `debug` log level.
+    *
+    * The codes targeted are:
+    * - `1005` - ',' expected.
+    * - `2451` - redeclared block scope variable.
+    *
+    * @param {object}   data - Data.
+    *
+    * @param {import('typescript').Diagnostic}  data.diagnostic - Diagnostic to test.
+    *
+    * @param {Function}   data.diagnosticLog - Diagnostic logging helper.
+    *
+    * @returns {boolean} Filtered state.
+    */
+   compileDiagnosticFilter({ diagnostic, diagnosticLog })
+   {
+      if (DTSPluginSvelte.#regexSvelteFile.test(diagnostic?.file?.fileName) &&
+       (diagnostic.code === 1005 || diagnostic.code === 2451))
+      {
+         diagnosticLog(diagnostic, 'debug');
+         return true;
+      }
+   }
+
+   /**
+    * Transform any Svelte files via `svelte2tsx` before TSC compilation.
+    *
+    * @param {object}   data - Data.
+    *
+    * @param {import('@typhonjs-utils/logger-color').ColorLogger} logger -
+    *
+    * @param {Map<string, string>}  data.memoryFiles - Stores transformed code and temp paths.
+    *
+    * @param {import('@typhonjs-build-test/esm-d-ts').ProcessedConfig} data.processedConfig - Processed config from
+    *        `esm-d-ts` that contains the filepaths being compiled.
+    */
+   compileTransform({ logger, memoryFiles, processedConfig })
+   {
+      const { compileFilepaths, compilerOptions } = processedConfig;
+
+      let hasSvelte = false;
+
+      const rootDir = compilerOptions.rootDir;
+
+      for (let cntr = compileFilepaths.length; --cntr >= 0;)
+      {
+         const filepath = compileFilepaths[cntr];
+         if (filepath.endsWith('.svelte'))
+         {
+            const relativeFilepath = upath.relative(rootDir, filepath);
+
+            hasSvelte = true;
+
+            try
+            {
+               const code = fs.readFileSync(filepath, 'utf-8').toString();
+               const isTsFile = DTSPluginSvelte.#regexScriptIsTS.test(code);
+
+               const tempPath = `${filepath}${isTsFile ? '.ts' : '.js'}`;
+
+               const tsx = svelte2tsx(code, {
+                  filename: filepath,
+                  isTsFile,
+                  mode: 'dts',
+                  noSvelteComponentTyped: true
+               });
+
+               memoryFiles.set(tempPath, tsx.code);
+               compileFilepaths[cntr] = tempPath;
+
+               // Process script contents for JSDoc comments.
+               const match = DTSPluginSvelte.#regexScriptContent.exec(code);
+               if (match)
+               {
+                  const jsdocResult = JSDocCommentParser.processScript(match.groups.contents, filepath,
+                   relativeFilepath, logger);
+
+                  // TODO: Remove logging
+                  // console.log(`!!! DPS - compileTransform - relativeFilepath: ${relativeFilepath} - jsdocResult: `, jsdocResult)
+
+                  if (jsdocResult)
+                  {
+                     this.#componentComments.set(`${relativeFilepath}.d.ts`, jsdocResult);
+                  }
+               }
+            }
+            catch (err)
+            {
+               // TODO: handle error and remove file from compilation.
+               throw err;
+            }
+         }
+      }
+
+      if (hasSvelte)
+      {
+         const svelteShims = fileURLToPath(resolve('svelte2tsx/svelte-shims-v4.d.ts', import.meta.url));
+         compileFilepaths.push(upath.toUnix(svelteShims));
+      }
+   }
+
+   /**
+    * Compiles the Svelte component returning the JS code so that `es-module-lexer` can parse it.
+    *
+    * @param {object}   data - Data.
+    *
+    * @param {string}   data.fileData - Svelte component file to compile / transform
+    *
+    * @returns {string} Compiled JS section of Svelte component.
+    */
+   lexerTransform({ fileData })
+   {
+      const { js } = compile(fileData);
+      return js.code;
+   }
 
    /**
     * Svelte v4 types will add a triple slash reference `/// <reference types="svelte" />` for generated types.
@@ -112,98 +243,6 @@ export class DTSPluginSvelte
       }
    }
 
-   /**
-    * Transform any Svelte files via `svelte2tsx` before TSC compilation.
-    *
-    * @param {object}   data - Data.
-    *
-    * @param {import('@typhonjs-utils/logger-color').ColorLogger} logger -
-    *
-    * @param {Map<string, string>}  data.memoryFiles - Stores transformed code and temp paths.
-    *
-    * @param {import('@typhonjs-build-test/esm-d-ts').ProcessedConfig} data.processedConfig - Processed config from
-    *        `esm-d-ts` that contains the filepaths being compiled.
-    */
-   transformCompile({ logger, memoryFiles, processedConfig })
-   {
-      const { compileFilepaths, compilerOptions } = processedConfig;
-
-      let hasSvelte = false;
-
-      const rootDir = compilerOptions.rootDir;
-
-      for (let cntr = compileFilepaths.length; --cntr >= 0;)
-      {
-         const filepath = compileFilepaths[cntr];
-         if (filepath.endsWith('.svelte'))
-         {
-            const relativeFilepath = upath.relative(rootDir, filepath);
-
-            hasSvelte = true;
-
-            try
-            {
-               const code = fs.readFileSync(filepath, 'utf-8').toString();
-               const isTsFile = DTSPluginSvelte.#regexScriptIsTS.test(code);
-
-               const tempPath = `${filepath}${isTsFile ? '.ts' : '.js'}`;
-
-               const tsx = svelte2tsx(code, {
-                  filename: filepath,
-                  isTsFile,
-                  mode: 'dts',
-                  noSvelteComponentTyped: true
-               });
-
-               memoryFiles.set(tempPath, tsx.code);
-               compileFilepaths[cntr] = tempPath;
-
-               // Process script contents for JSDoc comments.
-               const match = DTSPluginSvelte.#regexScriptContent.exec(code);
-               if (match)
-               {
-                  const jsdocResult = JSDocCommentParser.processScript(match.groups.contents, filepath,
-                   relativeFilepath, logger);
-
-                  // TODO: Remove logging
-                  // console.log(`!!! DPS - transformCompile - relativeFilepath: ${relativeFilepath} - jsdocResult: `, jsdocResult)
-
-                  if (jsdocResult)
-                  {
-                     this.#componentComments.set(`${relativeFilepath}.d.ts`, jsdocResult);
-                  }
-               }
-            }
-            catch (err)
-            {
-               // TODO: handle error and remove file from compilation.
-               throw err;
-            }
-         }
-      }
-
-      if (hasSvelte)
-      {
-         const svelteShims = fileURLToPath(resolve('svelte2tsx/svelte-shims-v4.d.ts', import.meta.url));
-         compileFilepaths.push(upath.toUnix(svelteShims));
-      }
-   }
-
-   /**
-    * Compiles the Svelte component returning the JS code so that `es-module-lexer` can parse it.
-    *
-    * @param {object}   data - Data.
-    *
-    * @param {string}   data.fileData - Svelte component file to compile / transform
-    *
-    * @returns {string} Compiled JS section of Svelte component.
-    */
-   transformLexer({ fileData })
-   {
-      const { js } = compile(fileData);
-      return js.code;
-   }
-
    // Plugin manager registration ------------------------------------------------------------------------------------
 
    /**
@@ -213,13 +252,14 @@ export class DTSPluginSvelte
    {
       const eventbus = ev.eventbus;
 
-      const options = { guard: true, sync: true };
+      const options = { async: true };
 
       // Event registration in the order which the events are fired.
 
       eventbus.on('lifecycle:start', this.lifecycleStart, this, options);
-      eventbus.on('lexer:transform:.svelte', this.transformLexer, this, options);
-      eventbus.on('compile:transform', this.transformCompile, this, options);
+      eventbus.on('lexer:transform:.svelte', this.lexerTransform, this, options);
+      eventbus.on('compile:transform', this.compileTransform, this, options);
+      eventbus.on('compile:diagnostic:filter', this.compileDiagnosticFilter, this, options);
       eventbus.on('compile:end', this.postprocessDTS, this, options);
    }
 }
