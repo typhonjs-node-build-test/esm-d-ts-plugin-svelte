3896ae29ef37aed67e291c358dd38873e63dd563
typhonrt <support@typhonjs.io>
2024-02-15 03:45:28 -0800

    Add no script Svelte component.

--- test/fixture/src/javascript/valid/index.js
+++ test/fixture/src/javascript/valid/index.js
@@ -1,4 +1,5 @@
 export { default as AccessorsTrue } from './AccessorsTrue.svelte';
+export { default as NoScript }      from './NoScript.svelte';
 export { default as TJSTest }       from './sub/TJSTest.svelte';
 
 export * from './jsCode.js';
